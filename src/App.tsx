a6e387943a9ed870fefaf8c2157a62ca678bb7ac
Shimi Zacken <shimi.zacken@sesam.io>
2025-02-18 14:21:19 +0100

    fix svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import DefaultAvatar from './images/avatar.svg';
+import PipeIcon from './images/pipe.svg';
 import { JsonPanel } from './JsonPanel.view';
 import { PasswordMeterTextFieldContainer } from './PasswordMeterTextField.container';
 
@@ -7,6 +8,11 @@ export const App = () => {
   return (
     <div>
       <DefaultAvatar style={{ width: 50 }} />
+      <div style={{ height: 40, lineHeight: '18px' }}>
+        <a style={{ color: 'green' }}>
+          <PipeIcon />
+        </a>
+      </div>
       <h1>Hello parcel!</h1>
       <section>
         <JsonPanel />
